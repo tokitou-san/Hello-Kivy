cebfb3efc254327a1c60a45d428e1d31ce3a1ea9
tokitou-san <suneeth.since2005@gmail.com>
2023-07-22 23:21:22 +0530

    add styles for widgets

--- hello.py
+++ hello.py
@@ -11,8 +11,13 @@ class MyGridLayout(GridLayout):
         super(MyGridLayout, self).__init__(*args, **kwargs)
         # configure no:of cols
         self.cols = 1
+        self.row_force_default = True
+        self.row_default_height = 50
         # new grid for inputs
-        self.input_grids = GridLayout()
+        self.input_grids = GridLayout(
+            row_force_default = True,
+            row_default_height = 50
+        )
         self.input_grids.cols = 2
         # set name widget
         self.input_grids.add_widget(Label(text="Your name:"))
@@ -31,7 +36,14 @@ class MyGridLayout(GridLayout):
         self.add_widget(self.input_grids)
 
         # set button widget
-        self.submit_button = Button(text="Submit", on_press=self.on_press)
+        self.submit_button = Button(
+            text="Submit",
+            on_press=self.on_press,
+
+            # styling
+            size_hint_y = None,
+            height = 50
+        )
         self.add_widget(self.submit_button)
 
     def on_press(self, instance):
@@ -40,7 +52,9 @@ class MyGridLayout(GridLayout):
         place = self.place.text
 
         # set an response widget
-        self.add_widget(Label(text=f"Hi {name}! you're {age} years old and from {place}. Nice to meet you :)"))
+        self.add_widget(Label(
+            text=f"Hi {name}! you're {age} years old and from {place}. Nice to meet you :)")
+        )
 
 class MyApp(App):
     def build(self):
