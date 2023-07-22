2119f690c31561abba94d58caac515446feea977
tokitou-san <suneeth.since2005@gmail.com>
2023-07-22 22:29:27 +0530

    add gridlayout

--- hello.py
+++ hello.py
@@ -1,10 +1,24 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+
+class MyGridLayout(GridLayout):
+    def __init__(self, *args, **kwargs):
+        # initialize constructor
+        super(MyGridLayout, self).__init__(*args, **kwargs)
+        # configure no:of cols
+        self.cols = 2
+        # set name widget
+        self.add_widget(Label(text="Your name:"))
+        self.add_widget(TextInput(multiline=False))
+        # set age widget
+        
 
 class MyApp(App):
     def build(self):
-        return Label(text="Hello world")
+        return MyGridLayout()
 
 if __name__ == "__main__":
     MyApp().run()
