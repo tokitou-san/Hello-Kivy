15fcfa58f5926ab03d6cdb75d0b0133a22cdb128
tokitou-san <suneeth.since2005@gmail.com>
2023-07-23 00:13:06 +0530

    add `ObjectProperty`

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
 class MainGridLayout(Widget):
-    pass
+    name = ObjectProperty(None)
+    age = ObjectProperty(None)
+    place = ObjectProperty(None)
 
 class MainApp(App):
     def build(self):
