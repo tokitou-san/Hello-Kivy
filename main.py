7856b23640e3cc915800d06c0146d415898ae4ab
tokitou-san <suneeth.since2005@gmail.com>
2023-07-23 05:39:00 +0530

    update on_press function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import kivy
 from kivy.app import App
+from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 
@@ -7,6 +8,20 @@ class MainGridLayout(Widget):
     name = ObjectProperty(None)
     age = ObjectProperty(None)
     place = ObjectProperty(None)
+
+    def handle_press(self):
+        name = self.name.text
+        age = self.age.text
+        place = self.place.text
+        # add response widget
+        # self.response = Label(text=f"Hi {name}! you're {age} years old and from {place}")
+        # self.add_widget(self.response)
+        print(f"Hi {name}! you're {age} years old and from {place}")
+        # clear inputs
+        self.name.text = ""
+        self.age.text = ""
+        self.place.text = ""
+
 
 class MainApp(App):
     def build(self):
