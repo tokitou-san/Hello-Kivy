d33bde271f8ecdbc0a6c01db9c4f1ad42c3b84c2
tokitou-san <suneeth.since2005@gmail.com>
2023-07-23 17:00:17 +0530

    add image in app

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
 import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
+from kivy.lang import Builder
 
+Builder.load_file("image.kv")
 class MainGridLayout(Widget):
-    name = ObjectProperty(None)
-    age = ObjectProperty(None)
-    place = ObjectProperty(None)
-
-    def handle_press(self) -> None:
-        name = self.name.text
-        age = self.age.text
-        place = self.place.text
-        # print response
-        print(f"Hi {name}! you're {age} years old and from {place}")
-        # clear inputs
-        self.handle_clear()
-
-    def handle_clear(self) -> None:
-        self.name.text = ""
-        self.age.text = ""
-        self.place.text = ""
+    pass
 
 
 class MainApp(App):
